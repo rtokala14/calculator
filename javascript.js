b5cdda0dd5483d7d3d7e825097c1da52560dfeb8
Rohit Tokala <rtokala@purdue.edu>
2022-05-31 19:48:27 +0400

    Complete operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            return addNumbers(num1, num2);
+            break;
+        case '-':
+            return subNumbers(num1, num2);
+            break;
+        case '*':
+            return multiNumbers(num1, num2);
+            break;
+        case '/':
+            return divNumbers(num1, num2);
+            break;
+        default:
+            return NaN;
+    }
+}
+
 function addNumbers(num1, num2) {
     return num1 + num2;
 }
