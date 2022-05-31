2e374449ded8f8c0d55b2d06a7f3e044837cfd43
Rohit Tokala <rtokala@purdue.edu>
2022-05-31 19:42:23 +0400

    Complete basic operator functions

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+function subNumbers(num1, num2) {
+    return num1 - num2;
+}
+
+function multiNumbers(num1, num2) {
+    return num1 * num2;
+}
+
+function divNumbers(num1, num2) {
+    return num1 / num2;
+}
